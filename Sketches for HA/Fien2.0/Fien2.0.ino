b268b795320e0a7595d0e9112282eb2147ce7897
gieljnssns <gieljnssns@me.com>
2017-11-11 13:21:38 +0100

    Initial

---
+++ Sketches for HA/Fien2.0/Fien2.0.ino
@@ -0,0 +1,160 @@
+/**
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ * REVISION HISTORY
+ * Version 1.0 - Henrik EKblad
+ * 
+ * DESCRIPTION
+ * This sketch provides an example how to implement a humidity/temperature
+ * sensor using DHT11/DHT-22 
+ * http://www.mysensors.org/build/humidity
+ */
+ 
+// Enable debug prints
+#define MY_DEBUG
+
+// Enable and select radio type attached
+#define MY_RADIO_NRF24
+//#define MY_RADIO_RFM69
+
+//#define MY_DEBUG_VERBOSE_RF24
+//#define MY_RF24_PA_LEVEL     RF24_PA_LOW
+//#define MY_RF24_DATARATE     RF24_1MBPS
+
+#define MY_NODE_ID 3
+
+#include <SPI.h>
+#include <MySensors.h>  
+#include <DHT.h>  
+
+#define SN "Fien"
+#define SV "2.0"
+#define CHILD_ID_LIGHT 0
+#define CHILD_ID_TEMP 1
+#define CHILD_ID_HUM 2
+#define CHILD_ID_MOT 3   // Id of the sensor child
+#define HUMIDITY_SENSOR_DIGITAL_PIN 4
+#define LIGHT_SENSOR_ANALOG_PIN 0
+#define DIGITAL_INPUT_SENSOR 3   // The digital input you attached your motion sensor.  (Only 2 and 3 generates interrupt!)
+#define INTERRUPT DIGITAL_INPUT_SENSOR-2 // Usually the interrupt = pin -2 (on uno/nano anyway)
+
+unsigned long SLEEP_TIME = 60000; // Sleep time between reads (in milliseconds)
+
+DHT dht;
+float lastTemp;
+float lastHum;
+boolean metric = true; 
+MyMessage msgHum(CHILD_ID_HUM, V_HUM);
+MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
+MyMessage msgMot(CHILD_ID_MOT, V_TRIPPED);
+MyMessage msgLight(CHILD_ID_LIGHT, V_LIGHT_LEVEL);
+int lastLightLevel;
+
+void presentation()  
+{ 
+  // Send the Sketch Version Information to the Gateway
+  sendSketchInfo(SN, SV);
+
+  // Register all sensors to gw (they will be created as child devices)
+  delay(200);
+  present(CHILD_ID_TEMP, S_TEMP);
+  delay(200);
+  present(CHILD_ID_HUM, S_HUM);
+  delay(200);
+  present(CHILD_ID_MOT, S_MOTION);
+  delay(200);
+  present(CHILD_ID_LIGHT, S_LIGHT_LEVEL);
+
+  metric = getConfig().isMetric;
+}
+
+void setup()  
+{ 
+  dht.setup(HUMIDITY_SENSOR_DIGITAL_PIN); 
+  if (SLEEP_TIME <= dht.getMinimumSamplingPeriod()) {
+    Serial.println("Warning: UPDATE_INTERVAL is smaller than supported by the sensor!");
+  }
+  // Sleep for the time of the minimum sampling period to give the sensor time to power up
+  // (otherwise, timeout errors might occure for the first reading)
+  sleep(dht.getMinimumSamplingPeriod());
+
+//  metric = getConfig().isMetric;
+
+  pinMode(DIGITAL_INPUT_SENSOR, INPUT);      // sets the motion sensor digital pin as input
+
+  delay(100);
+  send(msgTemp.set("0"));
+  delay(100);
+  send(msgHum.set("0"));
+  delay(100);
+  send(msgMot.set("0"));
+  delay(100);
+  send(msgLight.set("0"));
+}
+
+void loop()      
+{  
+  // Read digital motion value
+  boolean tripped = digitalRead(DIGITAL_INPUT_SENSOR) == HIGH; 
+        
+  Serial.println(tripped);
+  send(msgMot.set(tripped?"1":"0"));  // Send tripped value to gw 
+  
+  delay(dht.getMinimumSamplingPeriod());
+ 
+  // Fetch temperatures from DHT sensor
+  float temperature = dht.getTemperature();
+  if (isnan(temperature)) {
+      Serial.println("Failed reading temperature from DHT");
+  } else if (temperature != lastTemp) {
+    lastTemp = temperature;
+    if (!metric) {
+      temperature = dht.toFahrenheit(temperature);
+    }
+    send(msgTemp.set(temperature, 1));
+    #ifdef MY_DEBUG
+    Serial.print("T: ");
+    Serial.println(temperature);
+    #endif
+  }
+  
+  // Fetch humidity from DHT sensor
+  float humidity = dht.getHumidity();
+  if (isnan(humidity)) {
+      Serial.println("Failed reading humidity from DHT");
+  } else if (humidity != lastHum) {
+      lastHum = humidity;
+      send(msgHum.set(humidity, 1));
+      #ifdef MY_DEBUG
+      Serial.print("H: ");
+      Serial.println(humidity);
+      #endif
+  }
+  
+  // Fetch lightlevel from sensor
+  int lightLevel = (1023-analogRead(LIGHT_SENSOR_ANALOG_PIN))/10.23; 
+  Serial.println(lightLevel);
+  if (lightLevel != lastLightLevel) {
+      send(msgLight.set(lightLevel));
+      lastLightLevel = lightLevel;
+  }    
+  
+  sleep(INTERRUPT,CHANGE, SLEEP_TIME); //sleep a bit
+}
---
+++ config/custom_components/climate/netatmo.py
@@ -0,0 +1,190 @@
+"""
+Support for Netatmo Smart Thermostat.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/climate.netatmo/
+"""
+import logging
+from datetime import timedelta
+import voluptuous as vol
+
+from homeassistant.const import TEMP_CELSIUS, ATTR_TEMPERATURE
+from homeassistant.components.climate import (
+    STATE_HEAT, STATE_IDLE, ClimateDevice, PLATFORM_SCHEMA)
+from homeassistant.util import Throttle
+from homeassistant.loader import get_component
+import homeassistant.helpers.config_validation as cv
+
+DEPENDENCIES = ['netatmo']
+
+_LOGGER = logging.getLogger(__name__)
+
+CONF_RELAY = 'relay'
+CONF_THERMOSTAT = 'thermostat'
+
+# DEFAULT_AWAY_TEMPERATURE = 14
+# # The default offeset is 2 hours (when you use the thermostat itself)
+DEFAULT_TIME_OFFSET = 7200
+# # Return cached results if last scan was less then this time ago
+# # NetAtmo Data is uploaded to server every hour
+MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=300)
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_RELAY): cv.string,
+    vol.Optional(CONF_THERMOSTAT, default=[]):
+        vol.All(cv.ensure_list, [cv.string]),
+})
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Set up the NetAtmo Thermostat."""
+    netatmo = get_component('netatmo')
+    device = config.get(CONF_RELAY)
+
+    import lnetatmo
+    try:
+        data = ThermostatData(netatmo.NETATMO_AUTH, device)
+        for module_name in data.get_module_names():
+            if CONF_THERMOSTAT in config:
+                if config[CONF_THERMOSTAT] != [] and \
+                   module_name not in config[CONF_THERMOSTAT]:
+                    continue
+            add_devices([NetatmoThermostat(data, module_name, device)], True)
+    except lnetatmo.NoDevice:
+        return None
+
+
+class NetatmoThermostat(ClimateDevice):
+    """Representation a Netatmo thermostat."""
+
+    def __init__(self, data, module_name, device):
+        """Initialize the sensor."""
+        self._data = data
+        self._state = None
+        self._device = device
+        self._name = module_name
+        self.module_id = data.thermostatdata.moduleByName(module=module_name,
+                                                          device=device)['_id']
+        self.device_id = data.thermostatdata.deviceByName(device=device)['_id']
+        print(self.device_id)
+        print(self.module_id)
+        self._unique_id = "Netatmo_thermostat {0} - {1}".format(self._name,
+                                                                self.module_id)
+        # print(self._unique_id)
+        self._target_temperature = None
+        self._current_temperature = None
+        self._operation = None
+        self._away = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def unique_id(self):
+        """Return the unique ID for this sensor."""
+        return self._unique_id
+        print(self._unique_id)
+
+    @property
+    def state(self):
+        """Return the state of the device."""
+        return self.current_operation
+
+    @property
+    def temperature_unit(self):
+        """Return the unit of measurement."""
+        return TEMP_CELSIUS
+
+    @property
+    def current_temperature(self):
+        """Return the current temperature."""
+        # return self._data.thermostatdata.temp
+        # return self._data.thermostatdata.currentTemp(module=self._name)
+        return self._current_temperature
+
+    @property
+    def target_temperature(self):
+        """Return the temperature we try to reach."""
+        return self._target_temperature
+
+    @property
+    def current_operation(self):
+        """Return the current state of the thermostat."""
+        state = self._operation
+        if state == 0:
+            return STATE_IDLE
+        elif state == 100:
+            return STATE_HEAT
+
+    @property
+    def is_away_mode_on(self):
+        """Return true if away mode is on."""
+        return self._away
+
+    def turn_away_mode_on(self):
+        """Turn away on."""
+        mode = "away"
+        temp = None
+        self._data.thermostatdata.setthermpoint(mode, temp, endTimeOffset=None, device_id=self.device_id, module_id=self.module_id)
+        self._away = True
+
+    def turn_away_mode_off(self):
+        """Turn away off."""
+        mode = "program"
+        temp = None
+        self._data.thermostatdata.setthermpoint(mode, temp, endTimeOffset=None, device_id=self.device_id, module_id=self.module_id)
+        self._away = False
+
+    def set_temperature(self, **kwargs):
+        """Set new target temperature for 2 hours."""
+        temperature = kwargs.get(ATTR_TEMPERATURE)
+        if temperature is None:
+            return
+        mode = "manual"
+        self._data.thermostatdata.setthermpoint(
+            mode, temperature, DEFAULT_TIME_OFFSET, device_id=self.device_id, module_id=self.module_id)
+        self._target_temperature = temperature
+        self._away = False
+        self.update()
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        """Get the latest data from NetAtmo API and updates the states."""
+        self._data.update()
+        self._target_temperature = self._data.thermostatdata.setpoints(module=self._name, device=self._device)
+        # self._target_temperature = self._data.thermostatdata.setpoint_temp
+        self._away = self._data.thermostatdata.away(module=self._name, device=self._device) == 'away'
+        # self._away = self._data.thermostatdata.setpoint_mode == 'away'
+        self._current_temperature = self._data.thermostatdata.currentTemp(module=self._name, device=self._device)
+        self._operation = self._data.thermostatdata.operation(module=self._name, device=self._device)
+
+
+class ThermostatData(object):
+    """Get the latest data from Netatmo."""
+
+    def __init__(self, auth, device=None):
+        """Initialize the data object."""
+        self.auth = auth
+        self.thermostatdata = None
+        self.module_names = []
+        self.device = device
+
+    def get_module_names(self):
+        """Return all module available on the API as a list."""
+        self.update()
+        if not self.device:
+            for device in self.thermostatdata.modules:
+                for module in self.thermostatdata.modules[device].values():
+                    self.module_names.append(module['module_name'])
+        else:
+            for module in self.thermostatdata.modules[self.device].values():
+                self.module_names.append(module['module_name'])
+        return self.module_names
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        """Call the NetAtmo API to update the data."""
+        import lnetatmo
+        self.thermostatdata = lnetatmo.ThermostatData(self.auth)
