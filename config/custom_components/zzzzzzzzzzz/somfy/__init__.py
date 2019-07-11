92f1f37bc8cf15f1dbd6ca885441f8800bf4c3af
Giel Janssens <gieljnssns@me.com>
2019-07-11 16:22:43 +0200

    zzzzzzzzzzzzz

--- config/custom_components/zzzzzzzzzzz/somfy/__init__.py
+++ config/custom_components/zzzzzzzzzzz/somfy/__init__.py
@@ -10,17 +10,19 @@ from datetime import timedelta
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
-from homeassistant.components.http import HomeAssistantView
-from homeassistant.core import callback
-from homeassistant.helpers import discovery
+from homeassistant import config_entries
+from custom_components.somfy import config_flow
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_TOKEN
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.typing import HomeAssistantType
 from homeassistant.util import Throttle
 
 API = 'api'
 
 DEVICES = 'devices'
 
-REQUIREMENTS = ['pymfy==0.4.3']
+REQUIREMENTS = ['pymfy==0.5.1']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -31,19 +33,8 @@ DOMAIN = 'somfy'
 CONF_CLIENT_ID = 'client_id'
 CONF_CLIENT_SECRET = 'client_secret'
 
-NOTIFICATION_CB_ID = 'somfy_cb_notification'
-NOTIFICATION_OK_ID = 'somfy_ok_notification'
-NOTIFICATION_TITLE = 'Somfy Setup'
-
-ATTR_ACCESS_TOKEN = 'access_token'
-ATTR_REFRESH_TOKEN = 'refresh_token'
-ATTR_CLIENT_ID = 'client_id'
-ATTR_CLIENT_SECRET = 'client_secret'
-
 SOMFY_AUTH_CALLBACK_PATH = '/auth/somfy/callback'
 SOMFY_AUTH_START = '/auth/somfy'
-
-DEFAULT_CACHE_PATH = '.somfy'
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
@@ -55,88 +46,72 @@ CONFIG_SCHEMA = vol.Schema({
 SOMFY_COMPONENTS = ['cover']
 
 
-def setup(hass, config):
+async def async_setup(hass, config):
     """Set up the Somfy component."""
-    from pymfy.api.somfy_api import SomfyApi
+    if DOMAIN not in config:
+        return True
 
     hass.data[DOMAIN] = {}
 
-    # This is called to create the redirect so the user can Authorize Home .
-    redirect_uri = '{}{}'.format(
-        hass.config.api.base_url, SOMFY_AUTH_CALLBACK_PATH)
-    conf = config[DOMAIN]
-    api = SomfyApi(conf.get(CONF_CLIENT_ID),
-                   conf.get(CONF_CLIENT_SECRET),
-                   redirect_uri, hass.config.path(DEFAULT_CACHE_PATH))
-    hass.data[DOMAIN][API] = api
+    config_flow.register_flow_implementation(
+        hass, DOMAIN, config[DOMAIN][CONF_CLIENT_ID],
+        config[DOMAIN][CONF_CLIENT_SECRET])
 
-    if not api.token:
-        authorization_url, _ = api.get_authorization_url()
-        hass.components.persistent_notification.create(
-            'In order to authorize Home Assistant to view your Somfy devices'
-            ' you must visit this <a href="{}" target="_blank">link</a>.'
-            .format(authorization_url),
-            title=NOTIFICATION_TITLE,
-            notification_id=NOTIFICATION_CB_ID
-        )
-        hass.http.register_view(SomfyAuthCallbackView(config))
-    else:
-        update_all_devices(hass)
-        for component in SOMFY_COMPONENTS:
-            discovery.load_platform(hass, component, DOMAIN, {}, config)
+    hass.async_create_task(
+        hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={'source': config_entries.SOURCE_IMPORT},
+        ))
 
     return True
 
 
-class SomfyAuthCallbackView(HomeAssistantView):
-    """Handle OAuth finish callback requests."""
+async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
+    """Set up Somfy from a config entry."""
+    def token_saver(token):
+        _LOGGER.debug('Saving updated token')
+        entry.data[CONF_TOKEN] = token
+        hass.config_entries.async_update_entry(entry, data={**entry.data})
 
-    url = SOMFY_AUTH_CALLBACK_PATH
-    name = 'auth:somfy:callback'
-    requires_auth = False
+    # Force token update.
+    from pymfy.api.somfy_api import SomfyApi
+    hass.data[DOMAIN][API] = SomfyApi(
+        entry.data['refresh_args']['client_id'],
+        entry.data['refresh_args']['client_secret'],
+        token=entry.data[CONF_TOKEN],
+        token_updater=token_saver
+    )
 
-    def __init__(self, config):
-        """Initialize the OAuth callback view."""
-        self.config = config
+    await update_all_devices(hass)
 
-    @callback
-    def get(self, request):
-        """Finish OAuth callback request."""
-        from aiohttp import web
-        from oauthlib.oauth2 import MismatchingStateError
-        from oauthlib.oauth2 import InsecureTransportError
+    for component in SOMFY_COMPONENTS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(
+                entry, component))
 
-        hass = request.app['hass']
-        response = web.HTTPFound('/')
+    return True
 
-        try:
-            code = request.query.get('code')
-            hass.data[DOMAIN][API].request_token(code=code)
-            hass.async_add_job(setup, hass, self.config)
-            hass.components.persistent_notification.dismiss(NOTIFICATION_CB_ID)
-            hass.components.persistent_notification.create(
-                "Somfy has been successfully authorized!",
-                title=NOTIFICATION_TITLE,
-                notification_id=NOTIFICATION_CB_ID
-            )
-        except MismatchingStateError:
-            _LOGGER.error("OAuth state not equal in request and response.",
-                          exc_info=True)
-        except InsecureTransportError:
-            _LOGGER.error("Somfy redirect URI %s is insecure.", request.url,
-                          exc_info=True)
 
-        return response
+async def async_unload_entry(hass: HomeAssistantType, entry: ConfigEntry):
+    """Unload a config entry."""
+
+    if not hass.data[DOMAIN]:
+        hass.data.pop(DOMAIN)
+
+    for component in SOMFY_COMPONENTS:
+        await hass.config_entries.async_forward_entry_unload(
+            entry, component)
+
+    return True
 
 
 class SomfyEntity(Entity):
     """Representation of a generic Somfy device."""
 
-    def __init__(self, device, hass):
+    def __init__(self, device, api):
         """Initialize the Somfy device."""
-        self.hass = hass
         self.device = device
-        self.api = hass.data[DOMAIN][API]
+        self.api = api
 
     @property
     def unique_id(self):
@@ -148,19 +123,39 @@ class SomfyEntity(Entity):
         """Return the name of the device."""
         return self.device.name
 
-    def update(self):
-        update_all_devices(self.hass)
+    @property
+    def device_info(self):
+        """Return device specific attributes.
+        Implemented by platform classes.
+        """
+        return {
+            'identifiers': {(DOMAIN, self.unique_id)},
+            'name': self.name,
+            'model': self.device.type,
+            'via_hub': (DOMAIN, self.device.site_id),
+        }
+
+    async def async_update(self):
+        """Update the device with the latest data."""
+        await update_all_devices(self.hass)
         devices = self.hass.data[DOMAIN][DEVICES]
         self.device = next((d for d in devices if d.id == self.device.id),
                            self.device)
 
+    def has_capability(self, capability):
+        """Test if device has a capability."""
+        capabilities = self.device.capabilities
+        return bool([c for c in capabilities if c.name == capability])
+
 
 @Throttle(MIN_TIME_BETWEEN_UPDATES)
-def update_all_devices(hass):
-    """Update state of the device."""
+async def update_all_devices(hass):
+    """Update all the devices."""
     from requests import HTTPError
     try:
         data = hass.data[DOMAIN]
         data[DEVICES] = data[API].get_devices()
     except HTTPError:
-        _LOGGER.warning("Cannot update devices.", exc_info=True)
+        _LOGGER.warning("Cannot update devices", exc_info=True)
+        return False
+    return True
--- config/custom_components/zzzzzzzzzzz/somfy/cover.py
+++ config/custom_components/zzzzzzzzzzz/somfy/cover.py
@@ -1,109 +1,105 @@
 """
 Support for Somfy Covers.
-For more details about this platform, please refer to the documentation at
 
+For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/cover.somfy/
 """
 
 from homeassistant.components.cover import CoverDevice, ATTR_POSITION, \
     ATTR_TILT_POSITION
-# from homeassistant.components.somfy import DOMAIN, SomfyEntity
-from custom_components.somfy import DOMAIN, SomfyEntity, DEVICES
+from custom_components.somfy import DOMAIN, SomfyEntity, DEVICES, API
 
 DEPENDENCIES = ['somfy']
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up the Somfy cover platform."""
+    def get_covers():
+        """Retrieve covers."""
+        from pymfy.api.devices.category import Category
 
-    from pymfy.api.devices.category import Category
-    categories = {Category.ROLLER_SHUTTER.value, Category.INTERIOR_BLIND.value,
-                  Category.EXTERIOR_BLIND.value}
+        categories = {Category.ROLLER_SHUTTER.value,
+                      Category.INTERIOR_BLIND.value,
+                      Category.EXTERIOR_BLIND.value}
 
-    devices = hass.data[DOMAIN][DEVICES]
-    for cover in devices:
-        if categories & set(cover.categories):
-            add_entities([SomfyCover(cover, hass)])
+        devices = hass.data[DOMAIN][DEVICES]
+
+        return [SomfyCover(cover, hass.data[DOMAIN][API]) for cover in
+                devices if
+                categories & set(cover.categories)]
+
+    async_add_entities(await hass.async_add_job(get_covers), True)
 
 
 class SomfyCover(SomfyEntity, CoverDevice):
     """Representation of a Somfy cover device."""
 
+    def __init__(self, device, api):
+        """Initialize the Somfy device."""
+        from pymfy.api.devices.blind import Blind
+        super().__init__(device, api)
+        self.cover = Blind(self.device, self.api)
+
+    async def async_update(self):
+        """Update the device with the latest data."""
+        from pymfy.api.devices.blind import Blind
+        await super().async_update()
+        self.cover = Blind(self.device, self.api)
+
     def close_cover(self, **kwargs):
         """Close the cover."""
-        from pymfy.api.devices.roller_shutter import RollerShutter
-        RollerShutter(self.device, self.api).close()
+        self.cover.close()
 
     def open_cover(self, **kwargs):
         """Open the cover."""
-        from pymfy.api.devices.roller_shutter import RollerShutter
-        RollerShutter(self.device, self.api).open()
+        self.cover.open()
 
     def stop_cover(self, **kwargs):
-        """Stop the cover"""
-        from pymfy.api.devices.roller_shutter import RollerShutter
-        RollerShutter(self.device, self.api).stop()
+        """Stop the cover."""
+        self.cover.stop()
 
     def set_cover_position(self, **kwargs):
         """Move the cover shutter to a specific position."""
-        position = kwargs.get(ATTR_POSITION)
-        from pymfy.api.devices.roller_shutter import RollerShutter
-        RollerShutter(self.device, self.api).set_position(100 - position)
+        self.cover.set_position(100 - kwargs[ATTR_POSITION])
 
     @property
     def current_cover_position(self):
         """Return the current position of cover shutter."""
         position = None
-        try:
-            from pymfy.api.devices.roller_shutter import RollerShutter
-            shutter = RollerShutter(self.device, self.api)
-            position = 100 - shutter.get_position()
-        except StopIteration:
-            pass
+        if self.has_capability('position'):
+            position = 100 - self.cover.get_position()
         return position
 
     @property
     def is_closed(self):
         """Return if the cover is closed."""
         is_closed = None
-        try:
-            from pymfy.api.devices.roller_shutter import RollerShutter
-            is_closed = RollerShutter(self.device, self.api).is_closed()
-        except StopIteration:
-            pass
+        if self.has_capability('position'):
+            is_closed = self.cover.is_closed()
         return is_closed
 
     @property
     def current_cover_tilt_position(self):
         """Return current position of cover tilt.
-        
         None is unknown, 0 is closed, 100 is fully open.
         """
         orientation = None
-        try:
-            from pymfy.api.devices.blind import Blind
-            orientation = Blind(self.device, self.api).orientation
-        except StopIteration:
-            pass
+        if self.has_capability('rotation'):
+            orientation = 100 - self.cover.orientation
         return orientation
 
     def set_cover_tilt_position(self, **kwargs):
         """Move the cover tilt to a specific position."""
-        orientation = kwargs.get(ATTR_TILT_POSITION)
-        from pymfy.api.devices.blind import Blind
-        Blind(self.device, self.api).orientation = orientation
+        self.cover.orientation = kwargs[ATTR_TILT_POSITION]
 
     def open_cover_tilt(self, **kwargs):
         """Open the cover tilt."""
-        from pymfy.api.devices.blind import Blind
-        Blind(self.device, self.api).orientation = 100
+        self.cover.orientation = 100
 
     def close_cover_tilt(self, **kwargs):
         """Close the cover tilt."""
-        from pymfy.api.devices.blind import Blind
-        Blind(self.device, self.api).orientation = 0
+        self.cover.orientation = 0
 
     def stop_cover_tilt(self, **kwargs):
         """Stop the cover."""
-        from pymfy.api.devices.blind import Blind
-        Blind(self.device, self.api).stop()
+        self.cover.stop()
